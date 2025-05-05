f2f43e65d9783b383cbeb45a48df594c0fe69975
ClopenSet <feidylan23@gmail.com>
2025-05-06 04:50:33 +0800

    feat: Add formula greet

---
+++ Formula/greet.rb
@@ -0,0 +1,29 @@
+class Greet < Formula
+  desc "A greeting program that remembers what you tell it"
+  homepage "https://github.com/ClopenSet/greet"
+  url "https://github.com/ClopenSet/greet/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "db68e9650ec9d412364fa27e30578e7c62f9f215ee775955e5d8ce22d5d228e6"
+  version "0.1.0"
+  license "Unlicense"
+
+  depends_on "boost"
+
+  def install
+    # 编译程序
+    system "g++", "-std=c++11", "src/greet.cpp", "-o", "greet", "-lboost_system", "-lboost_filesystem"
+
+    # 将编译后的可执行文件安装到 libexec
+    libexec.install "greet"
+
+    # 安装资源文件
+    (libexec/"resources").install "resourses/greet.txt"
+
+    # 创建包装脚本，确保环境变量和路径正确
+    (bin/"greet").write_env_script libexec/"greet", GREET_STORAGE: "#{libexec}/resources"
+  end
+
+  test do
+    system "#{bin}/greet", "change", "Hello, Test!"
+    assert_equal "Hello, Test!", shell_output("#{bin}/greet").strip
+  end
+end
