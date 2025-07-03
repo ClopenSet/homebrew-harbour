dd8c1d589ba7ce8bc746f38824aa91761272f37e
ClopenSet <feidylan23@gmail.com>
2025-07-04 04:39:36 +0800

    feat: modify hash for karastat-viewer

--- Formula/karastat-viewer.rb
+++ Formula/karastat-viewer.rb
@@ -2,7 +2,7 @@ class KarastatViewer < Formula
   desc "Keyboard heatmap viewer for KaraStat key logging data"
   homepage "https://github.com/ClopenSet/karastat-viewer"
   url "https://github.com/ClopenSet/karastat-viewer/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "eb33784b75c44374d4c96ea2d4474dac28f68ec124a4ceb939c362f4a5ba7f61"
+  sha256 "256b743bf98fff7a0d1a7eaff8cd3618f3fe50b8e674c94b2973f8ce6113376d"
   license "Unlicense"
 
   depends_on "go" => :build
