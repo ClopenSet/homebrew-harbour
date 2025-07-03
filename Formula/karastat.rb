c4aef9cc62bed197c260e57e5ee32ed9330ed53c
ClopenSet <feidylan23@gmail.com>
2025-07-04 04:10:09 +0800

    feat: upgrade karastat

--- Formula/karastat.rb
+++ Formula/karastat.rb
@@ -1,18 +1,13 @@
 class Karastat < Formula
   desc "A tool that tracks and stores how many times each key is pressed"
   homepage "https://github.com/ClopenSet/KaraStat"
-  url "https://github.com/ClopenSet/KaraStat.git", tag: "v1.0.1"
+  url "https://github.com/ClopenSet/KaraStat.git", tag: "v1.0.2"
   head "https://github.com/ClopenSet/KaraStat.git", branch: "main"
 
+  depends_on "karastat-viewer"
   depends_on "xcodegen" => :build
   depends_on :macos
 
-  bottle do
-    root_url "https://github.com/ClopenSet/KaraStat/releases/download/v1.0.1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e3aab6e811c4d8138edaede364897108ca03699b09d815ba2e64a4953944cdcf"
-  end
-  
   def install
     ENV.deparallelize
     system "make"
@@ -29,21 +24,26 @@ class Karastat < Formula
 
   def caveats
     <<~EOS
-      To begin using KaraStat, you must first run:
+      KaraStat is a background keypress tracking tool.
+
+      After installation, run:
 
         brew services start karastat
 
-      The first time you launch the service, macOS will prompt you to grant "Input Monitoring" access.
-      Please approve this request in System Settings > Privacy & Security > Input Monitoring.
+      On first launch, macOS will prompt you for "Input Monitoring" permission.
+      Approve it at:
+        System Settings > Privacy & Security > Input Monitoring
 
-      After granting access, you must restart the service:
+      After granting access, restart the service:
 
         brew services restart karastat
 
-      Once enabled, KaraStat will accurately track your physical keyboard keypresses.
-      All data is stored in a SQLite file located under:
-
+      All usage data is stored in:
         ~/Library/Application Support/karastat/
+
+      You can view statistics using:
+
+        karastat-viewer
     EOS
   end
 end
