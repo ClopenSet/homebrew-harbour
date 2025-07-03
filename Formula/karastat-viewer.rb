e38d12c55f8a86894289cdbf75615188dbe3da4e
ClopenSet <feidylan23@gmail.com>
2025-07-04 04:12:00 +0800

    feat: remove bottle support for karastat-viewer

--- Formula/karastat-viewer.rb
+++ Formula/karastat-viewer.rb
@@ -3,7 +3,7 @@ class KarastatViewer < Formula
   homepage "https://github.com/ClopenSet/karastat-viewer"
   url "https://github.com/ClopenSet/karastat-viewer/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "eb33784b75c44374d4c96ea2d4474dac28f68ec124a4ceb939c362f4a5ba7f61"
-  license "MIT"
+  license "Unlicense"
 
   depends_on "go" => :build
 
